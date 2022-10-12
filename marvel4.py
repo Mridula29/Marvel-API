8cb99433317b79922d0beb8ee4f1d6e6f2e7ce53
Mridula29 <109211626+Mridula29@users.noreply.github.com>
2022-10-12 14:02:08 +0000

    Add files via upload

--- marvel4.py
+++ marvel4.py
@@ -1,17 +1,25 @@
 from marvel3 import to_dataframe
 
 
-column = [['id','name','comics.available', 'series.available', 'stories.available', 'events.available']]
+#column = [['id','name','comics.available', 'series.available', 'stories.available', 'events.available']]
 
-def filter_conditions(to_dataframe, column, condition):
-    {condition:'condition'}
-    if condition is True:
-        return to_dataframe
+def filter_conditions(to_dataframe, column, condition,value):
+    if column == 'name':
+        condition = 'starts_with'
+        length = len(value)
+        return (to_dataframe[to_dataframe.name.str[:length] == value])
     else:
-        print("not available")
+        if condition == 'equal to':
+            return {to_dataframe[to_dataframe[column] == int(value)]}
+        elif condition == 'greater_than':
+            return {to_dataframe[to_dataframe[column] > int(value)]}
+        elif condition == 'less_than':
+            return {to_dataframe[to_dataframe[column] <int(value)]}
+        else:
+            return ("Not Available")
     
 
 
-filter_conditions(to_dataframe= to_dataframe, column= 'comics.available', condition = 10)
+filter_conditions(to_dataframe= to_dataframe, column= 'comics.available', condition='equal to', value= 5)
 print(filter_conditions)
     
